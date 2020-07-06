4252cb880b0ef5682646e76679b6d1167761ef0d
Judith Pillado <judith.pillado@gmail.com>
2020-07-05 20:14:53 -0500

    Add address

--- spec/features/shelters/shelter_names_spec.rb
+++ spec/features/shelters/shelter_names_spec.rb
@@ -2,14 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'shelters index page', type: :feature do
   it 'can see all shelter names' do
-    shelter_1 = Shelter.create(name: 'PAWS')
-    shelter_2 = Shelter.create(name: 'Animal Rescue Foundation')
-    shelter_3 = Shelter.create(name: 'Animal Welfare League')
+    shelter_1 = Shelter.create(name: 'PAWS',
+                               address: '1997 N Clybourn Ave',
+                               city: 'Chicago',
+                               state: 'IL',
+                               zip: '60614'
+                              )
+    shelter_2 = Shelter.create(name: 'Animal Rescue Foundation',
+                               address: '2890 Mitchell Drive',
+                               city: 'Walnut Creek',
+                               state: 'CA',
+                               zip: '94598'
+                              )
+    shelter_3 = Shelter.create(name: 'Animal Welfare League',
+                               address: '10305 Southwest Highway',
+                               city: 'Chicago Ridge',
+                               state: 'IL',
+                               zip: '60415'
+                              )
 
     visit '/shelters'
 
     expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content("Address: #{shelter_1.address}, #{shelter_1.city}, #{shelter_1.state} #{shelter_1.zip} ")
+
     expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content("Address: #{shelter_2.address}, #{shelter_2.city}, #{shelter_2.state} #{shelter_2.zip} ")
+
     expect(page).to have_content(shelter_3.name)
+    expect(page).to have_content("Address: #{shelter_3.address}, #{shelter_3.city}, #{shelter_3.state} #{shelter_3.zip} ")
   end
+
 end
